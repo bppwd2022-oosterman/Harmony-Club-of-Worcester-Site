cdf804740f896cbbdfa54debd39f0ca2b5da58bd
aoosterman <a.oosterman@baypath.net>
2021-10-21 10:32:16 -0400

    created the 4 new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   
   
+  resources :sponsors
+  resources :photos
+  resources :albums
+  resources :events
+  get 'site/home'
+  resources :pages
   devise_for :users
   resources :sections
-  root 'pages#show'
+  root 'site#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_122937) do
+ActiveRecord::Schema.define(version: 2021_10_21_130735) do
 
   create_table "pages", force: :cascade do |t|
     t.string "title"
